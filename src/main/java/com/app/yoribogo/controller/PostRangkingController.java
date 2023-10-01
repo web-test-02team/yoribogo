1a98287c1948b4ae1eee84c1804c6651cbba5314
NZ-love <irene801026@gmail.com>
2023-10-01 23:42:01 +0900

    수현1차수정본

---
+++ src/main/java/com/app/yoribogo/controller/PostRangkingController.java
@@ -0,0 +1,30 @@
+package com.app.yoribogo.controller;
+
+import com.app.yoribogo.domain.PostVO;
+import com.app.yoribogo.mapper.PostRangkingMapper;
+import com.app.yoribogo.service.PostRangkingService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/rangking/*")
+public class PostRangkingController {
+
+    private final PostRangkingService postRangkingService;
+
+    @GetMapping("rangking")
+    public void goToRangking(Model model){
+
+        List<PostVO> posts = postRangkingService.getByRangking();
+        model.addAttribute("posts", posts);
+
+    }
+}
